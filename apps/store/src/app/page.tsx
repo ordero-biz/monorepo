a96a3f4139a2c62b0d3e7a9ef102f9f0fc326f94
rsabadash <r.v.sabadash@gmail.com>
2026-03-29 23:46:24 +0300

    Normalize line endings to LF

--- apps/store/src/app/page.tsx
+++ apps/store/src/app/page.tsx
@@ -1,6 +1,14 @@
 import { buttonVariants } from '@ordero/ui';
 import Image from 'next/image';
 
+export function addThreeNumbers(
+  first: number,
+  second: number,
+  third: number,
+): number {
+  return first + second + third;
+}
+
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
