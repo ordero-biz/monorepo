77436cedd4549c2c77ea12cf0ab1c34bd959ee09
rsabadash <rsabadash@shutterstock.com>
2026-06-05 16:29:11 +0300

    Update tokens

--- apps/store/src/features/app-shell/BaseLayout.tsx
+++ apps/store/src/features/app-shell/BaseLayout.tsx
@@ -4,10 +4,10 @@ import type { BaseLayoutProps } from './types';
 
 const baseLayoutContentClassName = cn(
   'bg-background box-border mx-auto w-full max-w-full px-3 py-1',
-  'sm:max-w-[var(--breakpoint-sm-token)] sm:px-4',
-  'md:max-w-[var(--breakpoint-md-token)] md:px-5',
-  'lg:max-w-[var(--breakpoint-lg-token)] lg:px-6',
-  'xl:max-w-[var(--breakpoint-xl-token)] xl:px-7'
+  'sm:max-w-[var(--layout-container-sm)] sm:px-4',
+  'md:max-w-[var(--layout-container-md)] md:px-5',
+  'lg:max-w-[var(--layout-container-lg)] lg:px-6',
+  'xl:max-w-[var(--layout-container-xl)] xl:px-7'
 );
 
 export const BaseLayout = ({ children }: BaseLayoutProps) => (
