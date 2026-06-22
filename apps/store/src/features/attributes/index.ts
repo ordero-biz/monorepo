ab974617a3878bd340d0677b14fb7708c8947f44
rsabadash <r.v.sabadash@gmail.com>
2026-06-22 20:24:40 +0300

    Refactor file structure

--- apps/store/src/features/attributes/index.ts
+++ apps/store/src/features/attributes/index.ts
@@ -1,3 +1,2 @@
 export { AttributesList } from '@/features/attributes/AttributesList/AttributesList';
-export { AttributesListHeader } from '@/features/attributes/AttributesListHeader/AttributesListHeader';
-export { CreateAttributeDialog } from './CreateAttributeDialog/CreateAttributeDialog';
+export { AttributesListHeader } from '@/features/attributes/AttributesList/AttributesListHeader';
