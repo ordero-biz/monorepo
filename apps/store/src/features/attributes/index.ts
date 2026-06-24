48fe0749fb20b83ceeab8250f20d869e4b8b434a
dkostreba <kostrebadd@gmail.com>
2026-06-25 00:07:39 +0300

    Page header

--- apps/store/src/features/attributes/index.ts
+++ apps/store/src/features/attributes/index.ts
@@ -1,2 +1,4 @@
+export { AttributeDetail } from '@/features/attributes/AttributeDetail/AttributeDetail';
 export { AttributesList } from '@/features/attributes/AttributesList/AttributesList';
 export { AttributesListHeader } from '@/features/attributes/AttributesList/AttributesListHeader';
+export { CreateAttributeDialog } from '@/features/attributes/CreateAttributeDialog/CreateAttributeDialog';
