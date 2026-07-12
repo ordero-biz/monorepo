f8e613f9e093abe1c035f45928f91803e1896eba
rsabadash <r.v.sabadash@gmail.com>
2026-07-12 18:29:20 +0300

    Fix path

--- apps/store/src/features/products/index.ts
+++ apps/store/src/features/products/index.ts
@@ -1,3 +1,5 @@
 export { CreateProduct } from '@/features/products/list/CreateProduct';
-export { ProductsList } from '@/features/products/list/ProductsList/ProductsList';
-export { ProductsListHeader } from '@/features/products/list/ProductsList/ProductsListHeader';
+export {
+  ProductsList,
+  ProductsListHeader,
+} from '@/features/products/list/ProductsList';
